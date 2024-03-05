4bdd51428c54c4ca4379546175d0ff9a1df87a2a
AsifSabir <lishasif@gmail.com>
2024-03-05 15:07:46 +0500

    pages

---
+++ app/listing/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Listing from '../../components/Listing/Listing'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <div>
+            <Listing />
+        </div>
+    )
+}
+
+export default page
---
+++ app/user/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import User from '../../components/User/User'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <div>
+            <User />
+        </div>
+    )
+}
+
+export default page
